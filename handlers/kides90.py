ee2e481067d6c7208346a957a70fb2ee0997eb9b
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-16 00:42:21 +0530

    Update kides90.py

--- handlers/kides90.py
+++ handlers/kides90.py
@@ -6,7 +6,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from main import *
-from handlers.null import AioHttp
+from helpers.null import AioHttp
 from handlers.help import add_command_help
 
 
