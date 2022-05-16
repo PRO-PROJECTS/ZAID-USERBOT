bfc78ee2ac92b10fc6e23c001b5fb5e2668d4ec1
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-16 10:28:52 +0530

    Update global.py

--- handlers/global.py
+++ handlers/global.py
@@ -13,6 +13,136 @@ from helpers.SQL.gbandb import gban_info, gban_list, gban_user, ungban_user
 from helpers.SQL.gmutedb import gmute, is_gmuted, ungmute
 
 
+
+RAID = [
+    "MADARCHOD TERI MAA KI CHUT ME GHUTKA KHAAKE THOOK DUNGA 🤣🤣",
+    "TERE BEHEN K CHUT ME CHAKU DAAL KAR CHUT KA KHOON KAR DUGA",
+    "TERI VAHEEN NHI HAI KYA? 9 MAHINE RUK SAGI VAHEEN DETA HU 🤣🤣🤩",
+    "TERI MAA K BHOSDE ME AEROPLANEPARK KARKE UDAAN BHAR DUGA ✈️🛫",
+    "TERI MAA KI CHUT ME SUTLI BOMB FOD DUNGA TERI MAA KI JHAATE JAL KE KHAAK HO JAYEGI💣",
+    "TERI MAAKI CHUT ME SCOOTER DAAL DUGA👅",
+    "TERE BEHEN K CHUT ME CHAKU DAAL KAR CHUT KA KHOON KAR DUGA",
+    "TERE BEHEN K CHUT ME CHAKU DAAL KAR CHUT KA KHOON KAR DUGA",
+    "TERI MAA KI CHUT KAKTE 🤱 GALI KE KUTTO 🦮 ME BAAT DUNGA PHIR 🍞 BREAD KI TARH KHAYENGE WO TERI MAA KI CHUT",
+    "DUDH HILAAUNGA TERI VAHEEN KE UPR NICHE 🆙🆒😙",
+    "TERI MAA KI CHUT ME ✋ HATTH DALKE 👶 BACCHE NIKAL DUNGA 😍",
+    "TERI BEHN KI CHUT ME KELE KE CHILKE 🍌🍌😍",
+    "TERI BHEN KI CHUT ME USERBOT LAGAAUNGA SASTE SPAM KE CHODE",
+    "TERI VAHEEN DHANDHE VAALI 😋😛",
+    "TERI MAA KE BHOSDE ME AC LAGA DUNGA SAARI GARMI NIKAL JAAYEGI",
+    "TERI VAHEEN KO HORLICKS PEELAUNGA MADARCHOD😚",
+    "TERI MAA KI GAAND ME SARIYA DAAL DUNGA MADARCHOD USI SARIYE PR TANG KE BACHE PAIDA HONGE 😱😱",
+    "TERI MAA KO KOLKATA VAALE JITU BHAIYA KA LUND MUBARAK 🤩🤩",
+    "TERI MUMMY KI FANTASY HU LAWDE, TU APNI BHEN KO SMBHAAL 😈😈",
+    "TERA PEHLA BAAP HU MADARCHOD ",
+    "TERI VAHEEN KE BHOSDE ME XVIDEOS.COM CHALA KE MUTH MAARUNGA 🤡😹",
+    "TERI MAA KA GROUP VAALON SAATH MILKE GANG BANG KRUNGA🙌🏻☠️ ",
+    "TERI ITEM KI GAAND ME LUND DAALKE,TERE JAISA EK OR NIKAAL DUNGA MADARCHOD🤘🏻🙌🏻☠️ ",
+    "AUKAAT ME REH VRNA GAAND ME DANDA DAAL KE MUH SE NIKAAL DUNGA SHARIR BHI DANDE JESA DIKHEGA 🙄🤭🤭",
+    "TERI MUMMY KE SAATH LUDO KHELTE KHELTE USKE MUH ME APNA LODA DE DUNGA☝🏻☝🏻😬",
+    "TERI VAHEEN KO APNE LUND PR ITNA JHULAAUNGA KI JHULTE JHULTE HI BACHA PAIDA KR DEGI👀👯 ",
+    "TERI MAA KI CHUT MEI BATTERY LAGA KE POWERBANK BANA DUNGA 🔋 🔥🤩",
+    "TERI MAA KI CHUT MEI C++ STRING ENCRYPTION LAGA DUNGA BAHTI HUYI CHUT RUK JAYEGIIII😈🔥😍",
+    "TERI MAA KE GAAND MEI JHAADU DAL KE MOR 🦚 BANA DUNGAA 🤩🥵😱",
+    "TERI CHUT KI CHUT MEI SHOULDERING KAR DUNGAA HILATE HUYE BHI DARD HOGAAA😱🤮👺",
+    "TERI MAA KO REDI PE BAITHAL KE USSE USKI CHUT BILWAUNGAA 💰 😵🤩",
+    "BHOSDIKE TERI MAA KI CHUT MEI 4 HOLE HAI UNME MSEAL LAGA BAHUT BAHETI HAI BHOFDIKE👊🤮🤢🤢",
+    "TERI BAHEN KI CHUT MEI BARGAD KA PED UGA DUNGAA CORONA MEI SAB OXYGEN LEKAR JAYENGE🤢🤩🥳",
+    "TERI MAA KI CHUT MEI SUDO LAGA KE BIGSPAM LAGA KE 9999 FUCK LAGAA DU 🤩🥳🔥",
+    "TERI VAHEN KE BHOSDIKE MEI BESAN KE LADDU BHAR DUNGA🤩🥳🔥😈",
+    "TERI MAA KI CHUT KHOD KE USME CYLINDER ⛽️ FIT KARKE USMEE DAL MAKHANI BANAUNGAAA🤩👊🔥",
+    "TERI MAA KI CHUT MEI SHEESHA DAL DUNGAAA AUR CHAURAHE PE TAANG DUNGA BHOSDIKE😈😱🤩",
+    "TERI MAA KI CHUT MEI CREDIT CARD DAL KE AGE SE 500 KE KAARE KAARE NOTE NIKALUNGAA BHOSDIKE💰💰🤩",
+    "TERI MAA KE SATH SUAR KA SEX KARWA DUNGAA EK SATH 6-6 BACHE DEGI💰🔥😱",
+    "TERI BAHEN KI CHUT MEI APPLE KA 18W WALA CHARGER 🔥🤩",
+    "TERI BAHEN KI GAAND MEI ONEPLUS KA WRAP CHARGER 30W HIGH POWER 💥😂😎",
+    "TERI BAHEN KI CHUT KO AMAZON SE ORDER KARUNGA 10 rs MEI AUR FLIPKART PE 20 RS MEI BECH DUNGA🤮👿😈🤖",
+    "TERI MAA KI BADI BHUND ME ZOMATO DAL KE SUBWAY KA BFF VEG SUB COMBO [15cm , 16 inches ] ORDER COD KRVAUNGA OR TERI MAA JAB DILIVERY DENE AYEGI TAB USPE JAADU KRUNGA OR FIR 9 MONTH BAAD VO EK OR FREE DILIVERY DEGI🙀👍🥳🔥",
+    "TERI BHEN KI CHUT KAALI🙁🤣💥",
+    "TERI MAA KI CHUT ME CHANGES COMMIT KRUGA FIR TERI BHEEN KI CHUT AUTOMATICALLY UPDATE HOJAAYEGI🤖🙏🤔",
+    "TERI MAUSI KE BHOSDE MEI INDIAN RAILWAY 🚂💥😂",
+    "TU TERI BAHEN TERA KHANDAN SAB BAHEN KE LAWDE RANDI HAI RANDI 🤢✅🔥",
+    "TERI BAHEN KI CHUT MEI IONIC BOND BANA KE VIRGINITY LOOSE KARWA DUNGA USKI 📚 😎🤩",
+    "TERI RANDI MAA SE PUCHNA BAAP KA NAAM BAHEN KE LODEEEEE 🤩🥳😳",
+    "TU AUR TERI MAA DONO KI BHOSDE MEI METRO CHALWA DUNGA MADARXHOD 🚇🤩😱🥶",
+    "TERI MAA KO ITNA CHODUNGA TERA BAAP BHI USKO PAHCHANANE SE MANA KAR DEGA😂👿🤩",
+    "TERI BAHEN KE BHOSDE MEI HAIR DRYER CHALA DUNGAA💥🔥🔥",
+    "TERI MAA KI CHUT MEI TELEGRAM KI SARI RANDIYON KA RANDI KHANA KHOL DUNGAA👿🤮😎",
+    "TERI MAA KI CHUT ALEXA DAL KEE DJ BAJAUNGAAA 🎶 ⬆️🤩💥",
+    "TERI MAA KE BHOSDE MEI GITHUB DAL KE APNA BOT HOST KARUNGAA 🤩👊👤😍",
+    "TERI BAHEN KA VPS BANA KE 24*7 BASH CHUDAI COMMAND DE DUNGAA 🤩💥🔥🔥",
+    "TERI MUMMY KI CHUT MEI TERE LAND KO DAL KE KAAT DUNGA MADARCHOD 🔪😂🔥",
+    "SUN TERI MAA KA BHOSDA AUR TERI BAHEN KA BHI BHOSDA 👿😎👊",
+    "TUJHE DEKH KE TERI RANDI BAHEN PE TARAS ATA HAI MUJHE BAHEN KE LODEEEE 👿💥🤩🔥",
+    "SUN MADARCHOD JYADA NA UCHAL MAA CHOD DENGE EK MIN MEI ✅🤣🔥🤩",
+    "APNI AMMA SE PUCHNA USKO US KAALI RAAT MEI KAUN CHODNEE AYA THAAA! TERE IS PAPA KA NAAM LEGI 😂👿😳",
+    "TOHAR BAHIN CHODU BBAHEN KE LAWDE USME MITTI DAL KE CEMENT SE BHAR DU 🏠🤢🤩💥",
+    "TUJHE AB TAK NAHI SMJH AYA KI MAI HI HU TUJHE PAIDA KARNE WALA BHOSDIKEE APNI MAA SE PUCH RANDI KE BACHEEEE 🤩👊👤😍",
+    "TERI MAA KE BHOSDE MEI SPOTIFY DAL KE LOFI BAJAUNGA DIN BHAR 😍🎶🎶💥",
+    "TERI MAA KA NAYA RANDI KHANA KHOLUNGA CHINTA MAT KAR 👊🤣🤣😳",
+    "TERA BAAP HU BHOSDIKE TERI MAA KO RANDI KHANE PE CHUDWA KE US PAISE KI DAARU PEETA HU 🍷🤩🔥",
+    "TERI BAHEN KI CHUT MEI APNA BADA SA LODA GHUSSA DUNGAA KALLAAP KE MAR JAYEGI 🤩😳😳🔥",
+    "TOHAR MUMMY KI CHUT MEI PURI KI PURI KINGFISHER KI BOTTLE DAL KE TOD DUNGA ANDER HI 😱😂🤩",
+    "TERI MAA KO ITNA CHODUNGA KI SAPNE MEI BHI MERI CHUDAI YAAD KAREGI RANDI 🥳😍👊💥",
+    "TERI MUMMY AUR BAHEN KO DAUDA DAUDA NE CHODUNGA UNKE NO BOLNE PE BHI LAND GHUSA DUNGA ANDER TAK 😎😎🤣🔥",
+    "TERI MUMMY KI CHUT KO ONLINE OLX PE BECHUNGA AUR PAISE SE TERI BAHEN KA KOTHA KHOL DUNGA 😎🤩😝😍",
+    "TERI MAA KE BHOSDA ITNA CHODUNGA KI TU CAH KE BHI WO MAST CHUDAI SE DUR NHI JA PAYEGAA 😏😏🤩😍",
+    "SUN BE RANDI KI AULAAD TU APNI BAHEN SE SEEKH KUCH KAISE GAAND MARWATE HAI😏🤬🔥💥",
+    "TERI MAA KA YAAR HU MEI AUR TERI BAHEN KA PYAAR HU MEI AJA MERA LAND CHOOS LE 🤩🤣💥",
+    "TERI BHEN KI CHUT ME USERBOT LAGAAUNGA SASTE SPAM KE CHODE",
+    "TERI MAA KI GAAND ME SARIYA DAAL DUNGA MADARCHOD USI SARIYE PR TANG KE BACHE PAIDA HONGE 😱😱",
+    "TERI MAA KI CHUT ME ✋ HATTH DALKE 👶 BACCHE NIKAL DUNGA 😍",
+    "TERI BEHN KI CHUT ME KELE KE CHILKE 🤤🤤",
+    "TERI MAA KI CHUT ME SUTLI BOMB FOD DUNGA TERI MAA KI JHAATE JAL KE KHAAK HO JAYEGI💣💋",
+    "TERI VAHEEN KO HORLICKS PEELAKE CHODUNGA MADARCHOD😚",
+    "TERI ITEM KI GAAND ME LUND DAALKE,TERE JAISA EK OR NIKAAL DUNGA MADARCHOD😆🤤💋",
+    "TERI VAHEEN KO APNE LUND PR ITNA JHULAAUNGA KI JHULTE JHULTE HI BACHA PAIDA KR DEGI 💦💋",
+    "SUAR KE PILLE TERI MAAKO SADAK PR LITAKE CHOD DUNGA 😂😆🤤",
+    "ABE TERI MAAKA BHOSDA MADERCHOOD KR PILLE PAPA SE LADEGA TU 😼😂🤤",
+    "GALI GALI NE SHOR HE TERI MAA RANDI CHOR HE 💋💋💦",
+    "ABE TERI BEHEN KO CHODU RANDIKE PILLE KUTTE KE CHODE 😂👻🔥",
+    "TERI MAAKO AISE CHODA AISE CHODA TERI MAAA BED PEHI MUTH DIA 💦💦💦💦",
+    "TERI BEHEN KE BHOSDE ME AAAG LAGADIA MERA MOTA LUND DALKE 🔥🔥💦😆😆",
+    "RANDIKE BACHHE TERI MAAKO CHODU CHAL NIKAL",
+    "KITNA CHODU TERI RANDI MAAKI CHUTH ABB APNI BEHEN KO BHEJ 😆👻🤤",
+    "TERI BEHEN KOTO CHOD CHODKE PURA FAAD DIA CHUTH ABB TERI GF KO BHEJ 😆💦🤤",
+    "TERI GF KO ETNA CHODA BEHEN KE LODE TERI GF TO MERI RANDI BANGAYI ABB CHAL TERI MAAKO CHODTA FIRSE ♥️💦😆😆😆😆",
+    "HARI HARI GHAAS ME JHOPDA TERI MAAKA BHOSDA 🤣🤣💋💦",
+    "CHAL TERE BAAP KO BHEJ TERA BASKA NHI HE PAPA SE LADEGA TU",
+    "TERI BEHEN KI CHUTH ME BOMB DALKE UDA DUNGA MAAKE LAWDE",
+    "TERI MAAKO TRAIN ME LEJAKE TOP BED PE LITAKE CHOD DUNGA SUAR KE PILLE 🤣🤣💋💋",
+    "TERI MAAAKE NUDES GOOGLE PE UPLOAD KARDUNGA BEHEN KE LAEWDE 👻🔥",
+    "TERI MAAAKE NUDES GOOGLE PE UPLOAD KARDUNGA BEHEN KE LAEWDE 👻🔥",
+    "TERI BEHEN KO CHOD CHODKE VIDEO BANAKE XNXX.COM PE NEELAM KARDUNGA KUTTE KE PILLE 💦💋",
+    "TERI MAAAKI CHUDAI KO PORNHUB.COM PE UPLOAD KARDUNGA SUAR KE CHODE 🤣💋💦",
+    "ABE TERI BEHEN KO CHODU RANDIKE BACHHE TEREKO CHAKKO SE PILWAVUNGA RANDIKE BACHHE 🤣🤣",
+    "TERI MAAKI CHUTH FAADKE RAKDIA MAAKE LODE JAA ABB SILWALE 👄👄",
+    "TERI BEHEN KI CHUTH ME MERA LUND KAALA",
+    "TERI BEHEN LETI MERI LUND BADE MASTI SE TERI BEHEN KO MENE CHOD DALA BOHOT SASTE SE",
+    "BETE TU BAAP SE LEGA PANGA TERI MAAA KO CHOD DUNGA KARKE NANGA 💦💋",
+    "HAHAHAH MERE BETE AGLI BAAR APNI MAAKO LEKE AAYA MATH KAT OR MERE MOTE LUND SE CHUDWAYA MATH KAR",
+    "CHAL BETA TUJHE MAAF KIA 🤣 ABB APNI GF KO BHEJ",
+    "SHARAM KAR TERI BEHEN KA BHOSDA KITNA GAALIA SUNWAYEGA APNI MAAA BEHEN KE UPER",
+    "ABE RANDIKE BACHHE AUKAT NHI HETO APNI RANDI MAAKO LEKE AAYA MATH KAR HAHAHAHA",
+    "KIDZ MADARCHOD TERI MAAKO CHOD CHODKE TERR LIYE BHAI DEDIYA",
+    "JUNGLE ME NACHTA HE MORE TERI MAAKI CHUDAI DEKKE SAB BOLTE ONCE MORE ONCE MORE 🤣🤣💦💋",
+    "GALI GALI ME REHTA HE SAND TERI MAAKO CHOD DALA OR BANA DIA RAND 🤤🤣",
+    "SAB BOLTE MUJHKO PAPA KYOUNKI MENE BANADIA TERI MAAKO PREGNENT 🤣🤣",
+    "SUAR KE PILLE TERI MAAKI CHUTH ME SUAR KA LOUDA OR TERI BEHEN KI CHUTH ME MERA LODA",
+    "CHAL CHAL APNI MAAKI CHUCHIYA DIKA",
+    "HAHAHAHA BACHHE TERI MAAAKO CHOD DIA NANGA KARKE",
+    "TERI GF HE BADI SEXY USKO PILAKE CHOODENGE PEPSI",
+    "2 RUPAY KI PEPSI TERI MUMMY SABSE SEXY 💋💦",
+    "TERI MAAKO CHEEMS SE CHUDWAVUNGA MADERCHOOD KE PILLE 💦🤣",
+    "TERI BEHEN KI CHUTH ME MUTHKE FARAR HOJAVUNGA HUI HUI HUI",
+    "SPEED LAAA TERI BEHEN CHODU RANDIKE PILLE 💋💦🤣",
+    "ARE RE MERE BETE KYOUN SPEED PAKAD NA PAAA RAHA APNE BAAP KA HAHAH🤣🤣",
+    "SUN SUN SUAR KE PILLE JHANTO KE SOUDAGAR APNI MUMMY KI NUDES BHEJ",
+    "ABE SUN LODE TERI BEHEN KA BHOSDA FAAD DUNGA",
+    "TERI MAAKO KHULE BAJAR ME CHOD DALA 🤣🤣💋",
+]
+
+
 async def iter_chats(client: Client):
     """Iter Your All Chats"""
     chats = []
@@ -86,58 +216,6 @@ def get_text(message: Message) -> [None, str]:
     else:
         return None
 
-
-@Client.on_message(filters.me & filters.command("gmute", ["."]))
-async def gmute_him(client: Client, message: Message):
-    g = await message.edit_text("`Processing..`")
-    text_ = get_text(message)
-    user, reason = get_user(message, text_)
-    if not user:
-        await g.edit("`Reply To User Or Mention To Gmute Him`")
-        return
-    try:
-        userz = await client.get_users(user)
-    except:
-        await g.edit(f"`404 : User Doesn't Exists In This Chat !`")
-        return
-    if not reason:
-        reason = "Just_Gmutted!"
-    mee= await client.get_me()
-    if userz.id == mee.id:
-        await g.edit("`Are you kidding with ne`")
-        return
-    if await is_gmuted(userz.id):
-        await g.edit("`Re-Gmute? Seriously? :/`")
-        return
-    await gmute(userz.id, reason)
-    gmu = f"**#Gmutted** \n**User :** `{userz.id}` \n**Reason :** `{reason}`"
-    await g.edit(gmu)
-    
-
-
-@Client.on_message(filters.me & filters.command("ungmute", ["."]))
-async def gmute_him(client: Client, message: Message):
-    ug = await message.edit_text("`Processing..`")
-    text_ = get_text(message)
-    user_ = get_user(message, text_)[0]
-    if not user_:
-        await ug.edit("`Reply To User Or Mention To Un-Gmute Him`")
-        return
-    try:
-        userz = await client.get_users(user_)
-    except:
-        await ug.edit(f"`404 : User Doesn't Exists In This Chat !`")
-        return
-    mee= await client.get_me()
-    if userz.id == mee.id:
-        await ug.edit("`Are ya kidding with me`")
-        return
-    if not await is_gmuted(userz.id):
-        await ug.edit("`Un-Gmute A Non Gmutted User? Seriously? :/`")
-        return
-    await ungmute(userz.id)
-    ugmu = f"**#Un-Gmutted** \n**User :** `{userz.id}`"
-    await ug.edit(ugmu)
    
 
 
@@ -171,13 +249,8 @@ async def gbun_him(client: Client, message: Message):
         gbun.edit("`You Have No Chats! So Sad`")
         return
     await gbun.edit("`Starting GBans Now!`")
-    for devils in chat_dict:
-        try:
-            await client.kick_chat_member(devils, int(userz.id))
-        except:
-            failed += 1
     await gban_user(userz.id, reason)
-    gbanned = f"**#GBanned** \n**User :** [{userz.first_name}](tg://user?id={userz.id}) \n**Reason :** `{reason}` \n**Affected Chats :** `{chat_len-failed}`"
+    gbanned = f"Reply Raid has Been Activated to {userz}"
     await gbun.edit(gbanned)
     
 
@@ -209,11 +282,6 @@ async def ungbun_him(client: Client, message: Message):
         ungbun.edit("`You Have No Chats! So Sad`")
         return
     await ungbun.edit("`Starting Un-GBans Now!`")
-    for devils in chat_dict:
-        try:
-            await client.unban_chat_member(devils, int(userz.id))
-        except:
-            failed += 1
     await ungban_user(userz.id)
     ungbanned = f"**#Un_GBanned** \n**User :** [{userz.first_name}](tg://user?id={userz.id}) \n**Affected Chats :** `{chat_len-failed}`"
     await ungbun.edit(ungbanned)
@@ -227,62 +295,11 @@ async def watch(client: Client, message: Message):
     if not message.from_user:
         return
     user = message.from_user.id
-    if await is_gmuted(user):
-        try:
-            await message.delete()
-        except:
-            return
     if await gban_info(user):
-        if message.chat.type != "supergroup":
-            return
         try:
             me_ = await message.chat.get_member(int(client.me.id))
         except:
             return
-        if not me_.can_restrict_members:
-            return
-        try:
-            await client.kick_chat_member(message.chat.id, int(user))
-        except:
-            return
-        await client.send_message(
-            message.chat.id,
-            f"**#GbanWatch** \n**Chat ID :** `{message.chat.id}` \n**User :** `{user}` \n**Reason :** `{await gban_info(user)}`",
-        )
-    
+        await client.send_message(RAID)
 
 
-@Client.on_message(filters.me & filters.command("gbanlist", ["."]))
-async def give_glist(client: Client, message: Message):
-    oof = "**#GBanList** \n\n"
-    glist = await message.edit_text("`Processing..`")
-    list_ = await gban_list()
-    if len(list_) == 0:
-        await glist.edit("`No User is Gbanned Till Now!`")
-        return
-    for lit in list_:
-        oof += f"**User :** `{lit['user']}` \n**Reason :** `{lit['reason']}` \n\n"
-    await edit_or_send_as_file(oof, glist, client, "GbanList", "Gban-List")
-
-
-@Client.on_message(filters.me & filters.command("gcast", ["."]))
-async def gbroadcast(client: Client, message: Message):
-    msg_ = await message.edit_text("`Processing..`")
-    failed = 0
-    if not message.reply_to_message:
-        await msg_.edit("`Reply To Message Boss!`")
-        return
-    chat_dict = await iter_chats(client)
-    chat_len = len(chat_dict)
-    await msg_.edit("`Now Sending To All Chats Possible!`")
-    if not chat_dict:
-        msg_.edit("`You Have No Chats! So Sad`")
-        return
-    for c in chat_dict:
-        try:
-            msg = await message.reply_to_message.copy(c)
-        except:
-            failed += 1
-    await msg_.edit(
-        f"`Message Sucessfully Send To {chat_len-failed} Chats! Failed In {failed} Chats.`"
-    )
