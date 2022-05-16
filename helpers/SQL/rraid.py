d4e4eee980855fae5fbaa0a89b560c13096b39cd
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-16 11:25:31 +0530

    Update rraid.py

--- helpers/SQL/rraid.py
+++ helpers/SQL/rraid.py
@@ -3,11 +3,11 @@ from helpers.SQL import dbb
 Rbun = dbb["RBAN"]
 
 
-async def rban_user(user, reason="#MATHERCHOD"):
+async def rzaid(user, reason="#MATHERCHOD"):
     await Rbun.insert_one({"user": user, "reason": reason})
 
 
-async def rngban_user(user):
+async def unzaid(user):
     await Rbun.delete_one({"user": user})
 
 
@@ -15,7 +15,7 @@ async def rban_list():
     return [lo async for lo in Rbun.find({})]
 
 
-async def rban_info(user):
+async def rzaid_info(user):
     kk = await Rbun.find_one({"user": user})
     if not kk:
         return False
