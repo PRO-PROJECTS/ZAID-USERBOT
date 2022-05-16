b3c6843669baad0cf7a5659ab23479ce791075fe
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-16 10:04:36 +0530

    Create Rraid.py

---
+++ handlers/Rraid.py
@@ -0,0 +1,159 @@
+from pyrogram import filters
+from traceback import format_exc
+from typing import Tuple
+
+from pyrogram import Client
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    Message)
+
+from helpers.SQL.rraid import gmute, is_gmuted, ungmute
+
+
+async def iter_chats(client: Client):
+    """Iter Your All Chats"""
+    chats = []
+    async for dialog in client.iter_dialogs():
+        if dialog.chat.type in ["supergroup", "channel"]:
+            chats.append(dialog.chat.id)
+    return chats
+
+def get_user(message: Message, text: str) -> [int, str, None]:
+    """Get User From Message"""
+    if text is None:
+        asplit = None
+    else:
+        asplit = text.split(" ", 1)
+    user_s = None
+    reason_ = None
+    if message.reply_to_message:
+        user_s = message.reply_to_message.from_user.id
+        reason_ = text if text else None
+    elif asplit is None:
+        return None, None
+    elif len(asplit[0]) > 0:
+        if message.entities:
+            if len(message.entities) == 1:
+                required_entity = message.entities[0]
+                if required_entity.type == "text_mention":
+                    user_s = int(required_entity.user.id)
+                else:
+                    user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+        else:
+            user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+        if len(asplit) == 2:
+            reason_ = asplit[1]
+    return user_s, reason_
+
+
+async def edit_or_send_as_file(
+    text: str,
+    message: Message,
+    client: Client,
+    caption: str = "`Result!`",
+    file_name: str = "result",
+    parse_mode="md",
+):
+    """Send As File If Len Of Text Exceeds Tg Limit Else Edit Message"""
+    if not text:
+        await message.edit("`Wait, What?`")
+        return
+    if len(text) > 1024:
+        await message.edit("`OutPut is Too Large, Sending As File!`")
+        file_names = f"{file_name}.text"
+        open(file_names, "w").write(text)
+        await client.send_document(message.chat.id, file_names, caption=caption)
+        await message.delete()
+        if os.path.exists(file_names):
+            os.remove(file_names)
+        return
+    else:
+        return await message.edit(text, parse_mode=parse_mode)
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+
+@Client.on_message(filters.me & filters.command("gmute", ["."]))
+async def gmute_him(client: Client, message: Message):
+    g = await message.edit_text("`Processing..`")
+    text_ = get_text(message)
+    user, reason = get_user(message, text_)
+    if not user:
+        await g.edit("`Reply To User Or Mention To Gmute Him`")
+        return
+    try:
+        userz = await client.get_users(user)
+    except:
+        await g.edit(f"`404 : User Doesn't Exists In This Chat !`")
+        return
+    if not reason:
+        reason = "Just_Gmutted!"
+    mee= await client.get_me()
+    if userz.id == mee.id:
+        await g.edit("`Are you kidding with ne`")
+        return
+    if await is_gmuted(userz.id):
+        await g.edit("`Re-Gmute? Seriously? :/`")
+        return
+    await gmute(userz.id, reason)
+    gmu = f"**#Gmutted** \n**User :** `{userz.id}` \n**Reason :** `{reason}`"
+    await g.edit(gmu)
+    
+
+
+@Client.on_message(filters.me & filters.command("ungmute", ["."]))
+async def gmute_him(client: Client, message: Message):
+    ug = await message.edit_text("`Processing..`")
+    text_ = get_text(message)
+    user_ = get_user(message, text_)[0]
+    if not user_:
+        await ug.edit("`Reply To User Or Mention To Un-Gmute Him`")
+        return
+    try:
+        userz = await client.get_users(user_)
+    except:
+        await ug.edit(f"`404 : User Doesn't Exists In This Chat !`")
+        return
+    mee= await client.get_me()
+    if userz.id == mee.id:
+        await ug.edit("`Are ya kidding with me`")
+        return
+    if not await is_gmuted(userz.id):
+        await ug.edit("`Un-Gmute A Non Gmutted User? Seriously? :/`")
+        return
+    await ungmute(userz.id)
+    ugmu = f"**#Un-Gmutted** \n**User :** `{userz.id}`"
+    await ug.edit(ugmu)
+   
+
+
+
+@Client.on_message( ~filters.me & filters.incoming)
+async def watch(client: Client, message: Message):
+    if not message:
+        return
+    if not message.from_user:
+        return
+    user = message.from_user.id
+    if await is_gmuted(user):
+        try:
+            await message.reply_text(custom)
+        except:
+            return
+    
+
+
