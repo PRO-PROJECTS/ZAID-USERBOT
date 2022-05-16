276c1a9338120e9413ffc58fd46c3ef27c6b4b32
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-16 08:59:15 +0530

    Update Replyraid.py

--- handlers/Replyraid.py
+++ handlers/Replyraid.py
@@ -243,7 +243,7 @@ async def arr(client: Client, message: Message):
         e = b.id
         c = b.first_name
         username = f"[{c}](tg://user?id={e})"
-        event = await client.reply(message.chat_id, "Reply Raid Activating....")
+        event = await client.reply_text(message.chat_id, "Reply Raid Activating....")
         que[client] = []
         qeue = que.get(client)
         appendable = [e]
